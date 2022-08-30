85bd886eae9d549af6d8414680b12928f79a3fa4
Feodora Bako <105170258+csakafeda@users.noreply.github.com>
2022-08-30 12:50:47 +0200

    feat: header

--- re-cat/pages/_app.js
+++ re-cat/pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import Header from './navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return  <Component {...pageProps} />
 }
 
 export default MyApp
