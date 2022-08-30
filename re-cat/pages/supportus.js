d59cc7bb69defc1844fb17cc05fc85ec4a0e9877
Kraus Adam <krausadam21@gmail.com>
2022-08-30 14:18:03 +0200

    Add: Support us button in footer, create page for it

---
+++ re-cat/pages/supportus.js
@@ -0,0 +1,8 @@
+export default function Supportus(){
+   return(
+    <div>
+    <p>Tamogasd a ivartalanito ,tul szaporodas elleni kampanyunkat, az alabbi adoszamunkon. </p>
+    <p>                1 801 1476-1-13                </p>
+    </div>
+   ) 
+}
