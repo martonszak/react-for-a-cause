56b9383c4af00bc8315a69c8799826b9a2582053
efreetsultan <krizsan.robert.szv@gmail.com>
2022-08-31 09:02:04 +0200

    feature added mobile version of navbar

--- re-cat/pages/navbar.js
+++ re-cat/pages/navbar.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [likes, setLikes] = useState(0);
   const menu = [
     { text: "Főoldal", href: "/" },
     { text: "Rólunk", href: "/about" },
@@ -9,7 +11,22 @@ export default function Navbar() {
     { text: "Tippek", href: "/badreasons" },
   ];
 
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    if (count === 0 ) {
+      setCount(count + 1);
+    } 
+    if ( count === 1) {
+      setCount(count - 1);
+    }
+  }
+if (count === 0 ){
   return (
+    <div>
+    <div className="navbar-mobile">
+      <button className="navbar-button" onClick={handleClick}>| | |</button>
+    </div>
     <div className="navbar-container">
       <ul className="navbar-list">
         {menu.map((e, i) => (
@@ -21,5 +38,27 @@ export default function Navbar() {
         ))}
       </ul>
     </div>
+    </div>
   );
+}
+if ( count === 1) {
+  return (
+    <div>
+    <div className="navbar-mobile">
+      <button className="navbar-button-mobile" onClick={handleClick}>anyad</button>
+    </div>
+    <div className="navbar-container-mobile">
+      <ul className="navbar-list-mobile">
+        {menu.map((e, i) => (
+          <li className="navbar-list-item-mobile" key={i}>
+            <Link className="navbar-link-mobile" href={e.href}>
+              <a className="navbar-a-mobile">{e.text}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+    </div>
+  );
+}
 }
