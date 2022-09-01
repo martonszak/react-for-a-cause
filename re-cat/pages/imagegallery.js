b40c7f2cc2dfb37c64beb020f4e44d0b695c58a4
Marton Benjamin Szak <105061239+martonszak@users.noreply.github.com>
2022-09-01 20:36:13 +0200

    Style: Tricky (my cat) added

--- re-cat/pages/imagegallery.js
+++ re-cat/pages/imagegallery.js
@@ -1,5 +1,6 @@
 import ImageGallery from "react-image-gallery";
 
+import tricky from "../public/tricky.jpg"
 import cat_9 from "../public/9.jpg";
 import cat_10 from "../public/10.jpg";
 import cat_12 from "../public/12.jpg";
@@ -9,6 +10,7 @@ import cat_16 from "../public/16.jpg";
 import cat_17 from "../public/17.jpg";
 
 const images = [
+  { original: tricky.src },
   { original: cat_9.src },
   { original: cat_10.src },
   { original: cat_12.src },
