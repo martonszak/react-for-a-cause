97dc88bbc1d4292ba48498ef91466ac531ad9034
Kraus Adam <krausadam21@gmail.com>
2022-09-01 15:32:29 +0200

    Fix main page picture fill

--- re-cat/pages/goodmethod.js
+++ re-cat/pages/goodmethod.js
@@ -15,7 +15,7 @@ export default function GoodMethod() {
                     <p>A füzetből megismerhető az a nemzetközileg elfogadott és elterjedt módszer (TNR, trap-neuter-return, vagyis csapdázz, ivartalaníts, helyezd vissza) amelynek lényege a befogás, ivartalanítás és az eredeti élőhelyre történő visszahelyezés.
                         A téma kutatói felismerték, hogy ha egy területről eltávolítják az ott élő kóbor, félvad macskákat, a vákuum csak rövid ideig marad fenn, mert hamarosan új egyedek települnek a területre és kezdődik minden elölről.
                         Ha viszont az ivartalanított macskákat visszahelyezik a „lakóhelyükre”, az állomány stabilizálódik, nem lesznek új szaporulatok, a területen élő macskák távol tartják az idegen macskákat.
-                       <a className='pdfclass' href='http://macskakert.hu/feral.pdf'> A füzet pdf formátumban egy képre kattintva érhető el.</a> Papír alapú formában elfogyott, nem megrendelhető.
+                       <a className='pdfclass' href='http://macskakert.hu/feral.pdf'> A füzet pdf formátumban, egy alábbi képre kattintva érhető el.</a> Papír alapú formában elfogyott, nem megrendelhető.
                         A kiadvány végén található Felmérés a programról csak mintaként szolgál, nem beküldhető.
                         Az Universities Federation for Animal Welfare számos hasznos tanáccsal, felkészülési javaslattal, módszertani útmutatóval rendelkezik állatvédelmi témákban, érdemes áttekinteni a honlapjukat. </p>
                     <div classnName="linkcontainer">
